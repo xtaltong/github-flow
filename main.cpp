06d95fe7383eca20987aeb6f9a5dc34b9d0527db
xtaltong <ctong011@ucr.edu>
2020-10-27 23:29:20 -0700

    Added Word Count Function

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <string>
 
+int WordCount(char* str)
+{
+  if (str == NULL)
+  {
+    return 0;
+  }
+  int numWords = 0;
+  while(str != '\0')
+  {
+    numWords++;
+  }
+  return numWords;
+}
+
 int main()
 {
   std::string name;
+  std::string str;
   std::cout << "What is your name?" << std::endl;
   std::cin >> name;
   std::cout << "Hello " << name << "!" << std::endl;
+  std::cout << "What phrase would you like a word count of?" << std::endl;
+  std::cin >> str;
+  std::cout << "Word Count: " << WordCount(str);
   
   return 0;
 }
